c6aeccecc777c44b6a6c3dcc462c197d082308b1
Joao Arquimedes <joao.costa@synapsebrasil.com.br>
2022-08-05 16:37:51 -0300

    Ajuste script para renovar certificado

--- script.sh
+++ script.sh
@@ -21,7 +21,7 @@ function log(){
 }
 
 function cert_renew(){
-  docker compose run --rm certbot ash -c "certbot renew" && $0 test && $0 reload
+  docker compose run --rm certbot ash -c "certbot renew" && bash $0 test && bash $0 reload
 }
 
 $1
